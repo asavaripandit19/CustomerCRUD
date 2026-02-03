ccae9a80aa16f92cea4952eedc110e4d026fcdf9
asavaripandit19 <asavaripandit2020@gmail.com>
2026-02-03 12:29:59 +0530

    add

--- CustomerCRUD/src/main/java/com/account/demo/controller/CustomerController.java
+++ CustomerCRUD/src/main/java/com/account/demo/controller/CustomerController.java
@@ -10,12 +10,14 @@ import com.account.demo.model.Customer;
 import com.account.demo.service.CustomerService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CustomerController {
 
 	@Autowired
